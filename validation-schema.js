de8b7f66084c5ec58074dffaf1e40e94e579feb9
Dammy Kolawole <dammy.kolawole@gamesys.co.uk>
2017-02-22 17:35:03 +0000

    CSV Parser

---
+++ validation-schema.js
@@ -0,0 +1,17 @@
+	const Joi = require('joi');
+	
+    module.exports = Joi.object().keys({
+        channel: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(),
+		show: Joi.string().required(),
+		type: Joi.string().required(),
+		description: Joi.string(),
+		synopsis: Joi.string().allow(''),
+		date: Joi.date().required(),
+		startTime: Joi.string().required(),
+		endTime: Joi.string().required(),
+        season: Joi.number(),
+        episode: Joi.number(),
+        numberOfEpisodes: Joi.number(),
+        genre: Joi.string(),
+        frequency: Joi.string()        
+	});
